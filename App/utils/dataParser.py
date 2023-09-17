cdb24553341ba60738d7e591feab0de1d4157275
Dalton Edmisten <daltonedmisten@gmail.com>
2023-09-17 17:23:37 -0500

    Added parser

---
+++ App/utils/dataParser.py
@@ -0,0 +1,39 @@
+from typing import List
+from models import Clip
+
+def __read_file(filename) -> str:
+    
+    data: str = ""
+    with open(filename, 'r') as f:
+        data = f.read()
+    
+    return data
+
+def __get_sections(data: str) -> List[str]:
+    sections: List[str] = data.split("\n\n")
+
+    return sections
+
+def __parse_section(data: str) -> Clip:
+    lines = data.split("\n")
+
+    line_split = [line.split(": ") for line in lines]
+
+    line_data = {split[0]: split[1] for split in line_split}
+
+    clip: Clip = Clip()
+
+    clip.clipper = line_data.get("User", "Unknown")
+    clip.title = line_data.get("Title")
+    clip.link = line_data.get("Link")
+
+    return clip
+
+def __parse_sections(sections: List[str]) -> List[Clip]:
+    return [__parse_section(section) for section in sections]
+
+
+def parse_data(filename: str) -> List[Clip]:
+    data: str = __read_file(filename)
+    sections: List[str] = __get_sections(data)
+    return __parse_sections(sections)
