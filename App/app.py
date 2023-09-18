bf4e830eb82c889bbb4a8b693bbc84ef905cf289
ScorpioGK <scorpiogameking@gmail.com>
2023-09-17 20:06:33 -0400

    Breaks with uncommented parser line in app

--- App/app.py
+++ App/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from utils.dataParser import parse_data
 
 app = Flask(__name__)
 
+data = parse_data('App/Twitch Clips.txt')
+print (data)
+
 @app.route("/")
 def hello_world():
-    return "Hello World!"
+    return "hello clip"
